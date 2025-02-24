3fdbdef30109fcb83e93f1c5ca98b2d0958092bd
AndreFelipeAlmeida <andre.neves@ccc.ufcg.edu.br>
2025-02-24 13:55:20 -0300

    feat: criado structured output para o agente de arquitetura

---
+++ src/outputs.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel, Field
+from typing import Dict, List
+
+class Agent(BaseModel):
+    """
+    This class should be used to describe the agent that will be generated.
+    """
+    agent: str = Field(description="The name of the agent")
+    prompt: str = Field(description="The description of the agent")
+
+class ArchitectureOutput(BaseModel):
+    """
+    This output should be used to store the architecture of the multiagent system that will be generated.
+    """
+    agents: List[str] = Field(description="The agents of the multiagent system")
+    interactions: Dict[str, List[str]] = Field(description="The interactions between the agents, "
+                                                            "where the key is an agent and the value is a list of agents it interacts with.")
