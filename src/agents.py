f954b4ce4ad0fa66054586da35c2dfa976116c22
Cazans <caique.campelo@ccc.ufcg.edu.br>
2025-02-27 16:46:03 -0300

    refactor: mudança no prompt do agente assistente(analisador de requisitos), agora ele apenas reúne todas as informações e passa para o arquiteto

--- src/agents.py
+++ src/agents.py
@@ -14,37 +14,53 @@ import os
 load_dotenv(override=True)
 model = ChatOpenAI(model="gpt-4-turbo")
 
-def assistent_agent(state: AgentState) -> Command[Literal["human_node", "__end__"]]:
+def assistent_agent(state: AgentState) -> Command[Literal["human_node", "architect_agent"]]:
     system_prompt = """
-    You are an expert in multi-agent system architectures. When the user provides a system description, generate a structured architecture outlining agents, interactions, and execution flow. 
-    Transfer to end when the human says that they have finished the conversation.
-    
     Instruction:
-    You are a specialist in multi-agent system architectures. Based on the user's input, generate a detailed architecture.
-    RESPONSE ONLY IN LANGUAGE OF USER. 
-    ONLY!!!! reply if the message is related to the generation of multi-agent systems. Other topics will not be considered.
+    You are an expert in multi-agent system architectures. Based on the user's input, identify if there is any missing information and ask for clarification in a simple and objective way.
+    REPLY ONLY IN THE USER'S LANGUAGE.
+    ONLY!!!! respond if the message is related to the generation of multi-agent systems. Other topics will not be considered.
+    Move to the end when the human says he/she is done with the conversation.
 
     Input:
     The user will provide a description including:
-    - System Goal: What the system should solve or optimize.
-    - Types of Agents: If known, describe their roles.
-    - Agent Interactions: How they communicate and collaborate.
+    - System goal: What the system should solve or optimize.
     - Environment: Where the system operates and any constraints.
-    - Preferred Technologies: If applicable, mention frameworks, languages, or standards.
+    - Preferred technologies: If applicable, mention frameworks, languages ​​or patterns.
 
-    
-    Expected Output:
-    You must return an architecture that includes:
-    1. List of Agents and their responsibilities.
-    2. Communication Model (e.g., direct messaging, event-driven, blackboard system).
-    3. Execution Flow explaining how agents collaborate to achieve the goal.
-    4. Suggested Structural Diagram (e.g., UML, Entity-Relationship, Event Flow).
-    5. Recommended Technologies, including:
-    - Multi-agent frameworks (e.g., LangGraph, JADE, SPADE, OpenAI Autonode).
-    - Models of LLM (e.g., ChatGPT, Gemini, Llama)
+    Expected output:
+    You should return the information provided by the user, organized into topics:
+    1. System goal: What the system should solve or optimize.
+    2. Environment: Where the system operates and any constraints.
+    3. Preferred technologies: If applicable, mention frameworks, languages ​​or patterns.
 
-    If any information is unclear, ask the user for clarification before finalizing the architecture.
+    At the end of the interaction with the human, pass the collected information to "architect_agent" writing an "#architect_agent" code in the end.
     """
+    messages = state["messages"] + [SystemMessage(content=system_prompt)]
+    response = model.invoke(messages)
+    
+    messages.append(response)
+    if "#architect_agent" in response.content:
+        return Command(
+            update={
+                "messages" : messages,
+                "active_agent" : "architect_agent"
+            }, goto="human_node")
+    return Command(
+        update={
+                "messages" : messages,
+                "active_agent" : "assistent_agent"
+            }, goto="human_node")
+
+def architect_agent(state: AgentState) -> Command[Literal["human_node", "__end__"]]:
+    system_prompt = """
+    Instruction:
+    You are a multi-agent system architect. Based on the user's input, design a multi-agent system that meets the user's needs. 
+    You must return a description of the multi-agent system, including:
+    - Agents: The roles of each agent in the system.
+    - Communication: The communication protocol between agents.
+    """
+    
     messages = state["messages"] + [SystemMessage(content=system_prompt)]
     response = model.invoke(messages)
     
@@ -52,10 +68,10 @@ def assistent_agent(state: AgentState) -> Command[Literal["human_node", "__end__
     return Command(
         update={
                 "messages" : messages,
-                "active_agent" : "assistent_agent"
+                "active_agent" : "architect_agent"
             }, goto="human_node")
 
-def human_node(state: AgentState) -> Command[Literal['assistent_agent','__end__']]:
+def human_node(state: AgentState) -> Command[Literal['assistent_agent', 'architect_agent']]:
     """A node for collecting user input."""
     user_input = interrupt("Avalie a resposta do agente: ")
     active_agent = state["active_agent"]
