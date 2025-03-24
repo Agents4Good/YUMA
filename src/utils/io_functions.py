f96e4d5d415dd77f51d45e09724728b479ee737c
Caíque Calazans Campelo <116117098+Cazans@users.noreply.github.com>
2025-03-24 17:37:07 -0300

    refactor: ajuste para gerar um diretório que armazena os arquivos específicos gerados pelo sistema

--- src/utils/io_functions.py
+++ src/utils/io_functions.py
@@ -8,7 +8,11 @@ def print_graph(graph: CompiledStateGraph) -> None:
 
     graph_image = graph.get_graph().draw_mermaid_png()
 
-    image_path = os.path.join(os.getcwd(), "graph_image.png")
+    dir_path = os.getcwd() + "/generated_files"
+
+    os.makedirs(dir_path, exist_ok=True)
+
+    image_path = os.path.join(dir_path, "graph_image.png")
 
     with open(image_path, "wb") as f:
         f.write(graph_image)
