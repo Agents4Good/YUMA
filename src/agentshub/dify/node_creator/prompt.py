be2f11b6b2df8092374f862ce9d0ae5e0fe7d18c
Daniel Mantovani <103224655+dnielMantov@users.noreply.github.com>
2025-04-28 17:09:49 -0300

    feat: adicionado nó de agente aos agentes do GenIA

--- src/agentshub/dify/node_creator/prompt.py
+++ src/agentshub/dify/node_creator/prompt.py
@@ -6,9 +6,10 @@ NODE_CREATOR = """
     Fluxo de execução das ferramentas:
     1. `create_start_node(title: str, node_id: str)` - Cria o nó inicial do workflow responsável por capturar as entradas do usuário.
     2. `create_llm_node(title: str, node_id: str, role: str, context_variable: str, task: str, temperature: float)` - Cria um nó de agente (LLM) para um workflow multiagente.
-    3. `create_answer_node(title: str, node_id: str, answer_variables: List[str])` - Cria o nó final do workflow responsável por exibir os outputs.
-    4. `create_contains_logic_node(title: str, node_id: str, value: str, context_variable: str)` - Cria um nó de lógica que verifica se uma variável contém um valor específico.
-    5. `create_http_node(title: str, node_id: str)` - Cria um nó capaz de realizar requisições HTTPS.
+    3. `create_agent_node(title: str, node_id: str, instruction: str, context_variable: str, tool: Literal["web_scraper"], temperature: float)` - Cria um nó de agente ReAct que utiliza tools para um workflow multiagente.
+    4. `create_answer_node(title: str, node_id: str, answer_variables: List[str])` - Cria o nó final do workflow responsável por exibir os outputs.
+    5. `create_contains_logic_node(title: str, node_id: str, value: str, context_variable: str)` - Cria um nó de lógica que verifica se uma variável contém um valor específico.
+    6. `create_http_node(title: str, node_id: str)` - Cria um nó capaz de realizar requisições HTTPS.
 
     Retorne todas as chamadas de ferramentas (`tool_calls`) necessárias para construir a arquitetura do sistema.
     Você deve retornar a lista `tool_calls` no seguinte formato:
