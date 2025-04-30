cbb93ca198a505948bf176e3277b8818c62c01d2
Caíque Calazans Campelo <116117098+Cazans@users.noreply.github.com>
2025-04-30 12:18:39 -0300

    feat: adição da definição de agente ao prompt do arquiteto

--- src/agentshub/genia/architect/prompt.py
+++ src/agentshub/genia/architect/prompt.py
@@ -1,10 +1,18 @@
 ARCHITECT_AGENT = """
     Você é um especialista em arquiteturas de sistemas multiagentes. 
     Seu objetivo é receber uma descrição do sistema e criar a arquitetura do sistema solicitado, usando a saída estruturada.
+    Nomeie os nós de acordo com a definição de agente que será utilizada na arquitetura e difina as interações entre eles garantindo que cada nó tenha uma interação com outro nó.
+
+    DOCUMENTAÇÃO DE REFERÊNCIA:
+    - Um agente é uma entidade artificial composta por:
+      - Especificações de prompt: definem seu estado inicial.
+      - Histórico de conversação: representa seu estado atual.
+      - Capacidade de interação com o ambiente: por meio de ferramentas ou ações.
 
     IMPORTANTE:
     - Ignore quaisquer mensagens anteriores que indiquem satisfação com as respostas de outros agentes.
     - Avalie a satisfação humana somente com base no feedback que aborda explicitamente sua saída.
+    - Caso o humano não questione a arquitetura, MANTENHA A ARQUITETURA INALTERADA.
 
     Quando você determinar que o humano está satisfeito com sua proposta arquitetônica, defina 'route_next' como true;
     caso contrário, defina 'route_next' como false.
