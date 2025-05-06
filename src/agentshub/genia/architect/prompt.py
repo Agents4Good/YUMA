e9569ed80c986e6b3175eb58473fc363b1653709
Daniel Mantovani <103224655+dnielMantov@users.noreply.github.com>
2025-05-06 13:31:52 -0300

    refact: prompt do arquiteto
    Definindo apenas o Workflow para o arquiteto.

--- src/agentshub/genia/architect/prompt.py
+++ src/agentshub/genia/architect/prompt.py
@@ -26,15 +26,13 @@ ARCHITECT_AGENT = """
         - Nodes are the key components of a workflow. By connecting node with different functionalities, you can execute a series of operations within the workflow.
       - Variables:
         - Variables are used to link the input and output of node within a workflow, enabling complex processing logic throughout the process. Fore more details, please take refer to Variables.
-      - Chatflow and Workflow:
-        - Application Scenarios
-          - Chatflow: Designed for conversational scenarios, including customer service, semantic search, and other conversational applications that require multi-step logic in response construction.
-          - Workflow: Geared towards automation and batch processing scenarios, suitable for high-quality translation, data analysis, content generation, email automation, and more.
+      - Workflow:
+        - Designed for conversational scenarios, including customer service, semantic search, and other conversational applications that require multi-step logic in response construction.
       - Differences in Available Node:
         - The End node is an ending node for Workflow and can only be selected at the end of the process.
-        - The Answer node is specific to Chatflow, used for streaming text output, and can output at intermediate steps in the process.
-        - Chatflow has built-in chat memory (Memory) for storing and passing multi-turn conversation history, which can be enabled in node like LLM and question classifiers. Workflow does not have Memory-related configurations and cannot enable them.
-        - Built-in variables for Chatflow’s start node include: sys.query, sys.files, sys.conversation_id, sys.user_id. Built-in variables for Workflow’s start node include: sys.files, sys_id.
+        - The Answer node is used for streaming text output, and can output at intermediate steps in the process.
+        - Workflow has built-in chat memory (Memory) for storing and passing multi-turn conversation history, which can be enabled in node like LLM and question classifiers.
+        - Built-in variables for Workflow's start node include: sys.query, sys.files, sys.conversation_id, sys.user_id.
 
     IMPORTANTE:
     - Ignore quaisquer mensagens anteriores que indiquem satisfação com as respostas de outros agentes.
