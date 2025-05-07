2dd87fd045db18a44d7165a7bb04600ac2e7c05b
AndreFelipeAlmeida <andre.neves@ccc.ufcg.edu.br>
2025-05-07 11:12:14 -0300

    feat: tool node executando todas as tools dos nós de criação do dify

--- src/utils/dify/nodes.py
+++ src/utils/dify/nodes.py
@@ -26,7 +26,8 @@ def _write_dify_yaml(state: DifyState):
 
     file = Path(YAML_PATH)
     with open(file, "w") as outfile:
-        yaml.dump(yaml_dify, outfile, default_flow_style=False, allow_unicode=True)
+        yaml.dump(yaml_dify, outfile,
+                  default_flow_style=False, allow_unicode=True)
 
 
 def dify_yaml_builder(state: DifyState) -> Command:
@@ -69,7 +70,18 @@ tools_dify = {
 
 
 def call_dify_tools(state: DifyState) -> List[Command]:
-    tool_calls = state["messages"][-1].tool_calls
+    tool_calls = []
+    i = -1
+    while True:
+        tool_call = state["messages"][i].tool_calls
+        print("==============\ntool_call\n", tool_call)
+        print("============\nmessage\n", state["messages"][i])
+        if tool_call != []:
+            tool_calls.append(tool_call)
+        else:
+            break
+        i -= 1
+    print("===============\ntool_calls")
     print(tool_calls)
     commands = []
     for tool_call in tool_calls:
