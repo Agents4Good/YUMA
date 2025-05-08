5fd0f1a1ded0eaf5bffffc7454c040045a119c8b
AndreFelipeAlmeida <andre.neves@ccc.ufcg.edu.br>
2025-05-08 15:34:01 -0300

    feat: utils para extração de JSON e extração de tools do content da mensagem

---
+++ src/utils/extract_tools.py
@@ -0,0 +1,29 @@
+from langchain_core.messages import AIMessage
+from langchain_core.messages.base import BaseMessage
+import json
+import re
+
+
+def content_to_tool(message: BaseMessage):
+    pattern = r"<function=(\w+)(\{.*?\})<\/function>"
+    matches = re.findall(pattern, message.content)
+    print(matches)
+    result = []
+
+    for func_name, args_str in matches:
+        print("===============func_name\n", func_name)
+        print("===============args_str\n", args_str)
+
+        print("================")
+        try:
+            args = json.loads(args_str)
+        except json.JSONDecodeError:
+            continue
+
+        result.append({
+            'name': func_name,
+            'args': args,
+            'type': 'tool_call'
+        })
+    message.tool_calls = result
+    return message
