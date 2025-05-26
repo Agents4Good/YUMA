d7113892f37d13248729597423511a01b987273c
AndreFelipeAlmeida <andre.neves@ccc.ufcg.edu.br>
2025-05-26 13:28:30 -0300

    refact: usando agente genérico para o criador de edges

--- src/agentshub/dify/edge_creator/edge_creator.py
+++ src/agentshub/dify/edge_creator/edge_creator.py
@@ -2,18 +2,9 @@ from schema.dify import DifyState
 from langgraph.types import Command
 from .prompt import EDGE_CREATOR
 from models.dify import edge_creator_dify_model
-
+from agentshub import only_tools_agent
+from langchain_core.messages import SystemMessage
 
 # Agente responsável por criar as edges do sistema
 def edge_creator(state: DifyState) -> Command:
-    print("edge_creator")
-    system_prompt = EDGE_CREATOR
-
-    messages = state["messages"] + [system_prompt]
-    response = edge_creator_dify_model.invoke(messages)
-    print(response)
-    # tool call para adicionar os arcos no YAML
-    print("edge_creator executado")
-    return Command(
-        update={"messages": [response]},
-    )
+    return only_tools_agent(edge_creator_dify_model, SystemMessage(content=EDGE_CREATOR), state)
