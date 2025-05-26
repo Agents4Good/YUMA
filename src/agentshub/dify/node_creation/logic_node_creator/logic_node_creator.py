6fceb6b900390e569333ecf9f84d448e338d076e
Daniel Mantovani <103224655+dnielMantov@users.noreply.github.com>
2025-05-26 15:03:16 -0300

    feat/refact: state log em todo os agentes

--- src/agentshub/dify/node_creation/logic_node_creator/logic_node_creator.py
+++ src/agentshub/dify/node_creation/logic_node_creator/logic_node_creator.py
@@ -3,7 +3,10 @@ from .prompt import LOGIC_NODE_CREATOR
 from langgraph.types import Command
 from models.dify import logic_node_creator_model
 from agentshub import only_tools_agent
+from utils.genia import write_log_state
 
 
 def logic_node_creator(state: DifyState) -> Command:
-    return only_tools_agent(logic_node_creator_model, LOGIC_NODE_CREATOR, state)
+    _return = only_tools_agent(logic_node_creator_model, LOGIC_NODE_CREATOR, state)
+    write_log_state("logic_node_creator - return", _return)
+    return _return
