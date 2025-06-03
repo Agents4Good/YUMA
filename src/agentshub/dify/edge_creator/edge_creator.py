4d3005bdf08ccb96a93199d8f2c4af2b0d077b2d
Caíque Calazans Campelo <116117098+Cazans@users.noreply.github.com>
2025-06-03 16:37:13 -0300

    refact: adaptação dos creators ao novo modelo de chamada de tools

--- src/agentshub/dify/edge_creator/edge_creator.py
+++ src/agentshub/dify/edge_creator/edge_creator.py
@@ -3,17 +3,16 @@ from langgraph.types import Command
 from .prompt import EDGE_CREATOR
 from models.dify import edge_creator_dify_model
 from agentshub import only_tools_agent
-from langchain_core.messages import SystemMessage
-from utils.yuma import write_log_state, write_log
+from langchain_core.messages import SystemMessage, HumanMessage
+from utils.yuma import write_log_state
 
 
 # Agente responsável por criar as edges do sistema
 def edge_creator(state: DifyState) -> Command:
-    partial_state = {
-        "architecture": state["architecture_output"],
-        "nodes_dicts": state["nodes_dicts"]
-    }
-    write_log("edge_creator - STATE TEST", partial_state)
-    _return = only_tools_agent(edge_creator_dify_model, [SystemMessage(content=EDGE_CREATOR)], partial_state)
+    instruction = HumanMessage(content=f"Aqui está a arquitetura do sistema:\n{state['architecture_output'].model_dump_json()}" 
+                               + f"\n e os nodes criados:\n{state['nodes_dicts']}")
+
+    message = [SystemMessage(content=EDGE_CREATOR), instruction]
+    _return = only_tools_agent(edge_creator_dify_model, message)
     write_log_state("edge_creator - return", _return)
     return _return
