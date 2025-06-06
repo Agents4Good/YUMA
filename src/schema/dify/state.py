39fab124aaadee59f2bd6de4389ff7139910e2a7
AndreFelipeAlmeida <andre.neves@ccc.ufcg.edu.br>
2025-06-06 15:36:06 -0300

    feat: human_message do avaliador do YAML adicionado ao state

--- src/schema/dify/state.py
+++ src/schema/dify/state.py
@@ -2,7 +2,7 @@ from langgraph.graph import MessagesState
 from pydantic import Field
 from typing import Dict, Annotated
 import operator
-
+from langchain_core.messages import HumanMessage
 
 class DifyState(MessagesState):
     architecture_output: Dict = Field(
@@ -12,6 +12,11 @@ class DifyState(MessagesState):
         default=None,
         description="Stores the metadata of Dify YAML in a Dictionary format.",
     )
+    
+    human_message: HumanMessage = Field(
+        default=None, 
+        description="The instruction of what the agent have to do"),
+    
     nodes_dicts: Annotated[list, operator.add] = Field(
         default_factory=list,
         description="Stores the nodes of Dify YAML in a List of Dictionaries.",
